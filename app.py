f51c8fa96542418a859f24e50729f465fe0c687b
israelarcos <95376025+israelarcos@users.noreply.github.com>
2022-05-16 20:21:08 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from PySide2.QtWidgets import QApplication
+from controllers.main_window import ListGameWindow
+
+
+if __name__ == "__main__":
+    app = QApplication()
+    window = ListGameWindow()
+    window.show()
+
+    app.exec_()
+
